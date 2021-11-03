13ea676db96562708316328ae1f7dca7bae0caf5
karina.yesbukenova <karina.yesbukenova@gmail.com>
2021-11-03 17:56:11 +0600

    add first model examples

--- app/settings.py
+++ app/settings.py
@@ -18,6 +18,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+
+    'polymorphic',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
--- budget_manager/models.py
+++ budget_manager/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from polymorphic.models import PolymorphicModel
 
-# Create your models here.
+
+class Timestamp(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Account(models.Model):
+    CARD = 'card'
+    CASH = 'cash'
+
+    ACCOUNT_TYPE_CHOICES = [
+        (CARD, 'card'),
+        (CASH, 'cash'),
+    ]
+
+    user = models.ForeignKey(User, related_name='accounts', on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    type = models.CharField(
+        max_length=4,
+        choices=ACCOUNT_TYPE_CHOICES,
+        default=CARD
+    )
+
+
+class Category(PolymorphicModel):
+    name = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(max_length=15, unique=True)
+
+
+class IncomeCategory(Category):
+    pass
+
+
+class OutcomeCategory(Category):
+    pass
+
+
+class Income(Timestamp):
+    category = models.ForeignKey(IncomeCategory, related_name='incomes', on_delete=models.SET_NULL)
+    account = models.ForeignKey(Account, related_name='incomes', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class Outcome(Timestamp):
+    category = models.ForeignKey(OutcomeCategory, related_name='incomes', on_delete=models.SET_NULL)
+    account = models.ForeignKey(Account, related_name='incomes', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
